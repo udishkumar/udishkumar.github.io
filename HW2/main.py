e0e146b23960941156d56e70dcc661b0fe07e176
udishkumar <udishkum@usc.edu>
2023-10-04 19:30:05 -0700

    HW2 ready for submission

---
+++ HW2/main.py
@@ -0,0 +1,202 @@
+from flask import Flask, jsonify, render_template, request
+import requests
+import urllib.parse
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+EBAY_URL = "https://svcs.ebay.com/services/search/FindingService/v1"
+client_id = "UdishKum-pythonDe-PRD-a72a98ced-f8556164"
+
+@app.route('/', methods=['GET'])
+
+def index():
+    return render_template('index.html') 
+
+@app.route('/getEbayData', methods=['GET'])
+def get_ebay_data():
+    keywords = urllib.parse.unquote(request.args.get('keywords'))
+    currency = request.args.get('currency', 'USD')
+    payload = {
+        "OPERATION-NAME": "findItemsAdvanced",
+        "SERVICE-VERSION": "1.0.0",
+        "SECURITY-APPNAME": client_id,
+        "RESPONSE-DATA-FORMAT": "JSON",
+        "REST-PAYLOAD": "true",
+        "keywords": keywords
+        # "paginationInput.entriesPerPage": "100",
+    }
+
+    sortOrder = request.args.get('sortOrder')
+    if sortOrder:
+        payload.update({"sortOrder": sortOrder})
+
+    filter_counter = 0
+
+    filter_params = [
+        ('returnsAcceptedOnly', 'ReturnsAcceptedOnly'),
+        ('freeShippingOnly', 'FreeShippingOnly'),
+    ]
+
+    condition_values = request.args.get('condition')
+    if condition_values:
+        condition_values_list = condition_values.split(',')
+        payload[f"itemFilter({filter_counter}).name"] = "Condition"
+        for idx, value in enumerate(condition_values_list):
+            payload[f"itemFilter({filter_counter}).value({idx})"] = value
+        filter_counter += 1
+
+    for arg_name, ebay_name in filter_params:
+        value = request.args.get(arg_name)
+        if value:
+            payload.update({
+                f"itemFilter({filter_counter}).name": ebay_name,
+                f"itemFilter({filter_counter}).value": value
+            })
+            filter_counter += 1
+
+    price_params = [
+        ('minPrice', 'MinPrice'),
+        ('maxPrice', 'MaxPrice')
+    ]
+
+    for arg_name, ebay_name in price_params:
+        value = request.args.get(arg_name)
+        if value:
+            payload.update({
+                f"itemFilter({filter_counter}).name": ebay_name,
+                f"itemFilter({filter_counter}).value": value,
+                f"itemFilter({filter_counter}).paramName": "Currency",
+                f"itemFilter({filter_counter}).paramValue": currency
+            })
+            filter_counter += 1
+
+    
+    limit = request.args.get('limit')
+    if limit:
+        try:
+            limit = int(limit)
+        except ValueError:
+            return jsonify({"eBayResponse": {"responseCode": "1003", "responseMsg": "Invalid 'limit' parameter. Please provide a numeric value."}}), 400
+    else:
+        limit = None
+
+    response = requests.get(EBAY_URL, params=payload)
+    if response.status_code == 200:
+        data = response.json()
+
+        ack_status = data.get("findItemsAdvancedResponse", [{}])[0].get("ack", [""])[0]
+        if ack_status == "Failure":
+            ebay_error = data.get("findItemsAdvancedResponse", [{}])[0].get("errorMessage", [{}])[0].get("error", [{}])[0]
+            if ebay_error:
+                response_code = ebay_error.get("errorId", [""])[0]
+                response_msg = ebay_error.get("message", [""])[0]
+                return jsonify({"eBayResponse": {"responseCode": response_code, "responseMsg": 'Technical error encountered. '+response_msg}}), 400
+
+        total_entries = int(data["findItemsAdvancedResponse"][0]["paginationOutput"][0]["totalEntries"][0])
+        if not total_entries or total_entries == 0:
+            return jsonify({"eBayResponse": {"responseCode": "1001", "responseMsg": "No results found for given search criteria"}}), 200
+        
+        items_data = data["findItemsAdvancedResponse"][0]["searchResult"][0]["item"]
+        ebay_response_items = []
+
+        for item in items_data:
+                transformed_item = {
+                    "product_id": item["itemId"][0] if "itemId" in item else "",
+                    "product_name": item["title"][0] if "title" in item else "",
+                    "product_image": item["galleryURL"][0] if "galleryURL" in item else "",
+                    "product_url": item["viewItemURL"][0] if "viewItemURL" in item else "",
+                    "product_category": item["primaryCategory"][0]["categoryName"][0] if "primaryCategory" in item else "",
+                    "product_condition": item["condition"][0]["conditionDisplayName"][0] if "condition" in item else "",
+                    "product_price": float(item["sellingStatus"][0]["currentPrice"][0]["__value__"]) if "sellingStatus" in item else "",
+                    "top_rated": item["topRatedListing"][0] if "topRatedListing" in item else "",
+                    "shipping_cost": float(item["shippingInfo"][0]["shippingServiceCost"][0]["__value__"]) if "shippingInfo" in item and "shippingServiceCost" in item["shippingInfo"][0] and float(item["shippingInfo"][0]["shippingServiceCost"][0]["__value__"]) >= 0.01 else ""
+                }
+                ebay_response_items.append(transformed_item)
+
+        if limit is not None:
+            ebay_response_items = ebay_response_items[:limit]
+        else: pass
+
+        transformed_response = {
+            "productCount": total_entries,
+            "keyword": urllib.parse.unquote(keywords),
+            "eBayResponse": ebay_response_items
+        }
+
+        return jsonify(transformed_response)
+
+    else:
+        return jsonify({"eBayResponse": {"responseCode": "1002", "responseMsg": "Failed to fetch data from eBay Finding API"}}), response.status_code
+
+
+@app.route('/getEbayData/<item_id>', methods=['GET'])
+def get_ebay_data_by_id(item_id):
+    TOKEN_ENDPOINT = "https://api.ebay.com/identity/v1/oauth2/token"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic VWRpc2hLdW0tcHl0aG9uRGUtUFJELWE3MmE5OGNlZC1mODU1NjE2NDpQUkQtNzJhOThjZWQyZWM5LTMyYWEtNDc2OS1hNjA5LWVkOTU='
+    }
+    data = {
+        'grant_type': 'client_credentials',
+        'scope': 'https://api.ebay.com/oauth/api_scope'
+    }
+    token_response = requests.post(TOKEN_ENDPOINT, headers=headers, data=data)
+    
+    if token_response.status_code != 200:
+        return jsonify({"eBayResponse": {"responseCode": "1006", "responseMsg": "Failed to fetch data from eBay Shopping API"}}), token_response.status_code
+
+    access_token = token_response.json().get('access_token')
+    
+    SHOPPING_URL = "https://open.api.ebay.com/shopping"
+    header = {
+        'X-EBAY-API-IAF-TOKEN': access_token
+    }
+    payload = {
+        'callname': 'GetSingleItem',
+        'siteid': '0',
+        'version': '967',
+        'ItemID': item_id,
+        'IncludeSelector': 'Description,Details,ItemSpecifics',
+        'responseencoding': 'JSON',
+        'appid': client_id
+    }
+    
+    response = requests.get(SHOPPING_URL, headers=header, params=payload)
+    response_data = response.json()
+
+    if response_data.get("Ack") == "Failure":
+        ebay_error = response_data.get("Errors", [{}])[0]
+        responseCode = ebay_error.get("ErrorCode", "")
+        responseMessage = ebay_error.get("LongMessage", "")
+        return jsonify({
+            "eBayResponse": {
+                "responseCode": responseCode,
+                "responseMsg": responseMessage
+            }
+        }), 400
+
+    item = response_data["Item"]
+    item_specifics = [
+        {
+            "Name": nv.get("Name", ""),
+            "Value": nv["Value"][0] if isinstance(nv.get("Value", None), list) else nv.get("Value", "")
+        } for nv in item.get("ItemSpecifics", {}).get("NameValueList", [])
+    ]
+    ebay_response = {
+        "product_id": item.get("ItemID", ""),
+        "product_image": item.get("PictureURL", ["./static/styles/images/ebay_default.jpg"])[0],
+        "product_url": item.get("ViewItemURLForNaturalSearch", ""),
+        "product_name": item.get("Title", ""),
+        "product_price": f"{item.get('CurrentPrice', {}).get('Value', '')} {item.get('CurrentPrice', {}).get('CurrencyID', '')}",
+        "location": f"{item.get('Location', '')}, {item.get('PostalCode', '')}" if item.get("PostalCode") else item.get("Location", ""),
+        "seller": item.get("Seller", {}).get("UserID", ""),
+        "returnPolicy": f"{item.get('ReturnPolicy', {}).get('ReturnsAccepted', '')} within {item.get('ReturnPolicy', {}).get('ReturnsWithin', '')}",
+        "itemSpecifics": item_specifics
+    }
+
+    return jsonify(ebay_response)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1",port=8080, debug=True)
---
+++ HW2/static/javascript/script.js
@@ -0,0 +1,367 @@
+let minPriceValue = 0;
+let maxPriceValue = 0;
+let displayCount = 3;
+let currentSearchResults = null;
+let currentSearchKeyword = "";
+let currentSearchCount = 0;
+let mainDiv = document.getElementById('mainDiv');
+
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    logWithUUID("Fetching form data... and preventing default form validation")
+    logWithUUID("calling custom form validation...")
+    validatePriceRange();
+});
+
+function validatePriceRange() {
+    event.preventDefault();
+    logWithUUID("custom form validation starts...")
+    minPriceValue = parseFloat(document.getElementById('minId').value);
+    maxPriceValue = parseFloat(document.getElementById('maxId').value);
+    if (minPriceValue < 0 || maxPriceValue < 0) {
+        alert("Price range values cannot be negative! Please try a value greater than or equal to 0.");
+        logWithUUID("Price range values cannot be negative! Please try a value greater than or equal to 0.");
+        return false;
+    }
+
+    if (minPriceValue > maxPriceValue) {
+        alert("Oops! Lower price limit cannot be greater than upper price limit! Please try again.");
+        logWithUUID("Oops! Lower price limit cannot be greater than upper price limit! Please try again.");
+        return false;
+    }
+    displayCount = 3;
+    logWithUUID("custom form validation completed, proceeding to Flask APIs for requesting ebay data")
+    fetchDataFromEbay();
+    return false;
+}
+
+
+function fetchDataFromEbay() {
+    logWithUUID("Ebay data fetch request initiated...")
+    let xhr = new XMLHttpRequest();
+    keywords = document.getElementById('keyword-text').value;
+    if(keywords!= ""){
+        let selectedOptionId = document.querySelector('#sortBy option:checked').id;
+    let sortOrderMap = {
+        'BM': 'BestMatch',
+        'PHF': 'CurrentPriceHighest',
+        'BSHF': 'PricePlusShippingHighest',
+        'PSLF': 'PricePlusShippingLowest'
+    };
+    let sortOrder = sortOrderMap[selectedOptionId];
+    let returnsAcceptedOnly = document.getElementById('returnAccepted').checked;
+    let conditions = [
+        { id: 'new', value: 1000 },
+        { id: 'used', value: 3000 },
+        { id: 'veryGood', value: 4000 },
+        { id: 'good', value: 5000 },
+        { id: 'acceptable', value: 6000 },
+    ];
+
+    let conditionValues = conditions.filter(cond => document.getElementById(cond.id).checked).map(cond => cond.value);
+
+    let apiUrl = `https://myfirstpython-3979650766.wl.r.appspot.com/getEbayData?minPrice=${minPriceValue}&maxPrice=${maxPriceValue}&ReturnsAcceptedOnly=${returnsAcceptedOnly}&condition=${conditionValues.join(',')}&keywords=${keywords}&sortOrder=${sortOrder}`;
+
+    xhr.open('GET', apiUrl, true);
+    xhr.onload = function() {
+        if (this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            logWithUUID('ebay data fetched successfully!');
+
+            if (response.eBayResponse.responseCode) {
+                displayNoResultsMessage();
+                logWithUUID('No data fetched from ebay');
+            } else {
+                displayResults(response.eBayResponse, keywords, response.productCount);
+                logWithUUID('ebay data populated...');
+            }
+        } else {
+            displayNoResultsMessage();
+            logWithUUID('No results found displayed on frontend');
+        }
+    }
+    xhr.send();
+    logWithUUID('ebay data fetch request sent')
+    }
+}
+
+function displayNoResultsMessage() {
+    logWithUUID('Creating No results found message');
+    let prevOutput = document.querySelector(".output");
+    if (prevOutput) {
+        prevOutput.remove();
+    }
+
+    let outputDiv = document.createElement('div');
+    outputDiv.className = "output";
+
+    let message = document.createElement('h2');
+    message.className = "no-results-message";
+    message.innerText = "No Results found";
+    outputDiv.appendChild(message);
+
+    mainDiv.appendChild(outputDiv);
+}
+
+
+function displayResults(data, keyword, productCount) {
+    currentSearchResults = data;
+    currentSearchKeyword = keyword;
+    currentSearchCount = productCount;
+    let prevOutput = document.querySelector(".output");
+    if (prevOutput) {
+        prevOutput.remove();
+    }
+
+    let outputDiv = document.createElement('div');
+    outputDiv.className = "output";
+
+    let heading = document.createElement('h2');
+    heading.className = "results-found-heading";
+    heading.innerHTML = `${productCount} Results found for <i>${keyword}</i>`;
+    outputDiv.appendChild(heading);
+
+    let hr = document.createElement('hr');
+    hr.className = "results-found-hr";
+    outputDiv.appendChild(hr);
+
+    let slicedData = data.slice(0, displayCount);
+
+    slicedData.forEach(product => {
+        let card = document.createElement('div');
+        card.classList.add('product-card');
+        card.addEventListener('click', function() {
+           fetchProductDetails(product.product_id);
+           logWithUUID('Product details fetched for product id: '+product.product_id);
+        });
+        let imgDiv = document.createElement('div');
+        imgDiv.className = 'product-image';
+        let img = document.createElement('img');
+        img.src = product.product_image || "./static/styles/images/ebay_default.jpg";
+        imgDiv.appendChild(img);
+        
+        let detailsDiv = document.createElement('div');
+        detailsDiv.className = 'product-details';
+        let nameDiv = document.createElement('div');
+        nameDiv.className = 'product-name';
+        nameDiv.innerHTML = `<b class="bold-name">${product.product_name}</b>`;
+        
+        let categoryDiv = document.createElement('div');
+        categoryDiv.className = 'category';
+        categoryDiv.innerHTML = `Category: <i>${product.product_category}</i> `;
+        let categoryImg = document.createElement('img');
+        categoryImg.src = './static/styles/images/redirect.png';
+        categoryDiv.appendChild(categoryImg); 
+        categoryImg.addEventListener('click', function() {
+            event.stopPropagation();
+            logWithUUID('Redirecting to ebay product page');
+            window.open(product.product_url, '_blank');
+            logWithUUID('ebay product page opened in new tab for product id: '+product.product_id);
+        });
+
+        let conditionDiv = document.createElement('div');
+        conditionDiv.className = 'condition';
+        if (!product.product_condition) {
+            conditionDiv.style.display = "none";
+        }
+        if (product.product_condition) {
+            conditionDiv.innerText = `Condition: ${product.product_condition} `;
+            if (product.top_rated === "true") {
+                let topRatedImg = document.createElement('img');
+                topRatedImg.src = './static/styles/images/topRatedImage.png';
+                conditionDiv.appendChild(topRatedImg);
+                logWithUUID('Top rated product image added for product id: '+product.product_id);
+            }
+            detailsDiv.appendChild(conditionDiv);
+        }
+    
+        let priceDiv = document.createElement('div');
+
+        if (product.shipping_cost != "") {
+            priceDiv.innerHTML = `<b>Price: $${product.product_price} (+ $${product.shipping_cost} for shipping)</b>`;
+            logWithUUID('fetched product cost including shipping cost for product id: '+product.product_id);
+        } else {
+            priceDiv.innerHTML = `<b>Price: $${product.product_price}</b>`;
+            logWithUUID('fetched product cost only for product id: '+product.product_id);
+        }
+
+        detailsDiv.append(nameDiv, categoryDiv, conditionDiv, priceDiv);
+
+        card.append(imgDiv, detailsDiv);
+        outputDiv.appendChild(card);
+    });
+
+    if (displayCount === 3 && data.length > 3) {
+        outputDiv.appendChild(createButton("Show More", () => {
+            displayCount = 10;
+            displayResults(data, keyword, productCount);
+            logWithUUID('Card list expanded');
+        }));
+    } else if (displayCount === 10) {
+        outputDiv.appendChild(createButton("Show Less", () => {
+            displayCount = 3;
+            displayResults(data, keyword, productCount);
+            logWithUUID('Card list collapsed');
+        }));
+    }
+    mainDiv.appendChild(outputDiv);
+}
+
+function fetchProductDetails(productId) {
+    logWithUUID('Fetching product details for product id: '+productId);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://myfirstpython-3979650766.wl.r.appspot.com/getEbayData/${productId}`, true);
+    xhr.onload = function() {
+        if (this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            displayProductDetails(response);
+        } else {
+            displayNoResultsMessage();
+        }
+    };
+    xhr.send();
+}
+
+function displayProductDetails(data) {
+    logWithUUID('Displaying product details for product id: '+data.product_id);
+    let prevOutput = document.querySelector(".output");
+    if (prevOutput) {
+        prevOutput.remove();
+    }
+
+    let outputDiv = document.createElement('div');
+    outputDiv.className = "output";
+
+    let heading = document.createElement('h1');
+    heading.className = "item-details-heading";
+    heading.innerText = "Item Details";
+    outputDiv.appendChild(heading);
+
+    let backButton = createBackButton("Back to search results", () => {
+        displayResults(currentSearchResults, currentSearchKeyword, currentSearchCount);
+        logWithUUID('Moving back to search results page');
+    });
+
+    outputDiv.appendChild(backButton);
+
+    let table = document.createElement('table');
+    table.className = "item-details-table";
+    let fields = [
+        { name: 'Photo', value: `<img id="inlinePhoto" src="${data.product_image}" alt="Photo" width="100">`},
+        { name: 'eBay Link', value: `<a href="${data.product_url}" target="_blank">eBay Product Link</a>` },
+        { name: 'Title', value: data.product_name },
+        { name: 'Price', value: `${data.product_price}` },
+        { name: 'Location', value: data.location },
+        {name: 'Seller', value: data.seller},
+        { name: 'Return Policy (US)', value: data.returnPolicy}
+    ];
+
+    if (data.itemSpecifics && Array.isArray(data.itemSpecifics)) {
+        logWithUUID('Item specifications added for product id: '+data.product_id);
+        data.itemSpecifics.forEach(spec => {
+            fields.push({ name: spec.Name, value: spec.Value });
+        });
+    }
+
+    fields.forEach(item => {
+        let tr = document.createElement('tr');
+        
+        let tdName = document.createElement('td');
+        tdName.innerHTML = `<strong>${item.name}</strong>`;
+        tr.appendChild(tdName);
+        
+        let tdValue = document.createElement('td');
+        tdValue.innerHTML = item.value;
+        tr.appendChild(tdValue);
+        
+        table.appendChild(tr);
+    });
+
+    outputDiv.appendChild(table);
+    mainDiv.appendChild(outputDiv);
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+    logWithUUID('scrolling up');
+}
+
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+    logWithUUID('scrolling down');
+}
+
+function createBackButton(text, onClick) {
+    logWithUUID('Back to results button created');
+    let backButton = document.createElement('button');
+    backButton.innerText = text;
+    backButton.addEventListener('click', onClick);
+    return backButton;
+    
+}
+
+function createButton(text, onClick) {
+    let button = document.createElement('button');
+    button.innerText = text;
+    button.addEventListener('click', onClick);
+
+    if (text === "Show More") {
+        button.addEventListener('click', scrollToBottom);
+        logWithUUID('Displaying Show more button now');
+    } else if (text === "Show Less") {
+        button.addEventListener('click', scrollToTop);
+        logWithUUID('Displaying Show less button now');
+    }
+    return button;
+}
+
+function createButtonArea(data, keyword, productCount) {
+    let buttonArea = document.createElement('div');
+    buttonArea.className = "button-area";
+    if (displayCount === 3 && data.length > 3) {
+        buttonArea.appendChild(createButton("Show More", () => {
+            displayCount = 10;
+            displayResults(data, keyword, productCount);
+        }));
+    } else if (displayCount === 10) {
+        buttonArea.appendChild(createButton("Show Less", () => {
+            displayCount = 3;
+            displayResults(data, keyword, productCount);
+        }));
+    }
+    return buttonArea;
+}
+
+function generateUUID() { 
+    let d = new Date().getTime();
+    let d2 = (performance && performance.now && (performance.now()*1000)) || 0;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16;
+        if(d > 0){
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+function logWithUUID(message) {
+    const id = generateUUID();
+    console.log(`[ID:${id}] ${message}`);
+}
+
+logWithUUID("This is a message");
+
+document.getElementById('clearBtn').addEventListener('click', () => {
+    logWithUUID('Reset form to default values');
+    location.reload();
+});
